7ddc34a3df1bd9e12cfd435825d02cddb3b95847
Đam Sa <20521154@ms.uit.edu.vn>
2023-06-20 16:47:00 +0700

    deletePopover

--- src/layouts/components/CustomerHeader/index.jsx
+++ src/layouts/components/CustomerHeader/index.jsx
@@ -35,20 +35,7 @@ function HeaderCustomer({ children }) {
                 </Link>
                 Thuần Chay
             </div>
-            <Popover className="relative">
-                <Popover.Button>Solutions</Popover.Button>
 
-                <Popover.Panel className="absolute z-10">
-                    <div className="grid grid-cols-2">
-                        <a href="/analytics">Analytics</a>
-                        <a href="/engagement">Engagement</a>
-                        <a href="/security">Security</a>
-                        <a href="/integrations">Integrations</a>
-                    </div>
-
-                    <img src="/solutions.jpg" alt="" />
-                </Popover.Panel>
-            </Popover>
             <div className="flex space-x-3  mx-20">
                 {LINK.map((tab, index) => (
                     <Link
